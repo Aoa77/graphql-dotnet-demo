2865b14d9a3679da68e499acdf216a71df643703
aoa77 <alalbers77@gmail.com>
2024-03-27 12:17:11 -0500

    add GraphQL demo code

--- src/graphql.demo.app/Program.cs
+++ src/graphql.demo.app/Program.cs
@@ -1,9 +1,40 @@
 ﻿namespace graphql.demo.app;
 
-internal class Program
+using System;
+using GraphQL;
+using GraphQL.Execution;
+using GraphQL.Types;
+
+internal static class Program
 {
-    static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("===========================");
+        Console.WriteLine(Console.Title = "= GraphQL Dotnet Demo App =");
+        Console.WriteLine("===========================");
+        Console.WriteLine();
+
+        var schema = Schema.For(@"
+          type Query {
+              hello: String
+          }
+        ");
+
+        var root = new { Hello = "Hello World!" };
+        var json = await new DocumentExecuter().ExecuteAsync(_ =>
+        {
+            _.Schema = schema;
+            _.Query = "{ hello }";
+            _.Root = root;
+        });
+        var data = (RootExecutionNode?)json.Data;
+
+        Console.WriteLine($@"Query: {json.Query}");
+        Console.WriteLine($@"Result: {data!.Result}");
+
+        Console.WriteLine();
+        Console.WriteLine("===========================");
+        Console.WriteLine("press any key to exit...");
+        Console.ReadKey();
     }
 }
